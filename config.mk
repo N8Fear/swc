20167d95067fdfe61ea398cc7afc19b6c22363a8
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2015-10-03 15:14:59 +0200

    Change PREFIX

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 
 # The commented out options are defaults
 
-# PREFIX          = /usr/local
+ PREFIX          = /usr
 # BINDIR          = $(PREFIX)/bin
 # LIBDIR          = $(PREFIX)/lib
 # INCLUDEDIR      = $(PREFIX)/include
@@ -10,7 +10,7 @@
 # PKGCONFIGDIR    = $(LIBDIR)/pkgconfig
 
 CC              = gcc
-CFLAGS          = -pipe
+CFLAGS          = -pipe -march=native -O2
 # OBJCOPY         = objcopy
 # PKG_CONFIG      = pkg-config
 # WAYLAND_SCANNER = wayland-scanner
